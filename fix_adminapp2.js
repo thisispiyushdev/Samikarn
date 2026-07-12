eeff3a2b6879bf92e0c08b52a75d34e52f95e51b
Vivek Verma <166729635+vivekisadev@users.noreply.github.com>
2026-07-12 15:36:02 +0530

    chore: Remove unused temporary scripts and ui folder from root

--- fix_adminapp2.js
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-
-const file = './frontend/src/admin/AdminApp.jsx';
-let content = fs.readFileSync(file, 'utf8');
-
-content = content.replace(/`([A-Z]+)'/g, "'$1'");
-
-fs.writeFileSync(file, content);
-console.log("Fixed AdminApp.jsx again");
